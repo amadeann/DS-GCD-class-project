d11ea04495d7ab4d72ab293dedee40d7082f33f6
amadeann <annissimo@gmail.com>
2015-10-17 13:12:54 +0200

    updating files

--- analysis.R
+++ analysis.R
@@ -33,7 +33,7 @@ if(!file.exists(exDirName)) {
 
 # Load training and test datasets
 
-trainFile <- "UCI HAR Dataset/train/X_test.txt"
+trainFile <- "UCI HAR Dataset/train/X_train.txt"
 trainData <- read.table(trainFile, nrows = 6)
 
 testFile <- "UCI HAR Dataset/test/X_test.txt"
@@ -47,4 +47,34 @@ features <- as.vector(read.table(featuresFile, colClasses = c("numeric", "charac
 # Return a logical vector. Value is TRUE, when the variable name contains mean() or std() string.
 
 selectedFeatures <- as.vector(sapply(features,function(x) (length(grep("(mean\\(\\)|std\\(\\))",x))>0)))
-features[selectedFeatures]
+
+cleanColNames <- gsub(pattern="-", replacement = "_", features[selectedFeatures])
+cleanColNames <- gsub(pattern="\\(\\)", replacement = "", cleanColNames)
+
+# Limit the variables in the train and test datasets
+
+trainData <- trainData[selectedFeatures] 
+testData <- testData[selectedFeatures] 
+
+colnames(trainData) <- cleanColNames
+colnames(testData) <- cleanColNames
+
+# Load and add subject and activity info for training and test datasets
+
+subjectTrainFile <- "UCI HAR Dataset/train/subject_train.txt"
+subjectTestFile <- "UCI HAR Dataset/test/subject_test.txt"
+activityTrainFile <- "UCI HAR Dataset/train/y_train.txt"
+activityTestFile <- "UCI HAR Dataset/test/y_test.txt"
+
+subjectTrain <- as.vector(read.table(subjectTrainFile, colClasses = c("factor")))
+subjectTest <- as.vector(read.table(subjectTestFile, colClasses = c("factor")))
+activityTrain <- as.vector(read.table(activityTrainFile, colClasses = c("factor")))
+activityTest <- as.vector(read.table(activityTestFile, colClasses = c("factor")))
+
+# Add descriptive factor levels for activities. Use mapvalues() function from plyr package
+
+activityLabelsFile <- "UCI HAR Dataset/activity_labels.txt"
+activityLabels <- read.table(activityLabelsFile, colClasses = c("numeric", "character"), col.names = c("labNum", "label"))
+library(plyr)
+plyr::mapvalues(x, from = c("beta", "gamma"), to = c("two", "three"))
+
