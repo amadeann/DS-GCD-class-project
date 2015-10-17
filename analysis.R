cee5078d7d64dd2b8077624bd26e4a64bd566a5f
amadeann <annissimo@gmail.com>
2015-10-17 14:06:46 +0200

    updating files

--- analysis.R
+++ analysis.R
@@ -34,10 +34,10 @@ if(!file.exists(exDirName)) {
 # Load training and test datasets
 
 trainFile <- "UCI HAR Dataset/train/X_train.txt"
-trainData <- read.table(trainFile, nrows = 6)
+trainData <- read.table(trainFile)
 
 testFile <- "UCI HAR Dataset/test/X_test.txt"
-testData <- read.table(testFile, nrows = 6)
+testData <- read.table(testFile)
 
 # Load the features variables to filter variabiables for the clean data set
 
@@ -51,13 +51,10 @@ selectedFeatures <- as.vector(sapply(features,function(x) (length(grep("(mean\\(
 cleanColNames <- gsub(pattern="-", replacement = "_", features[selectedFeatures])
 cleanColNames <- gsub(pattern="\\(\\)", replacement = "", cleanColNames)
 
-# Limit the variables in the train and test datasets
+# Limit the variables in the train and test datasets using the selectedFeatures vector
 
 trainData <- trainData[selectedFeatures] 
 testData <- testData[selectedFeatures] 
-
-colnames(trainData) <- cleanColNames
-colnames(testData) <- cleanColNames
 
 # Load and add subject and activity info for training and test datasets
 
@@ -66,15 +63,33 @@ subjectTestFile <- "UCI HAR Dataset/test/subject_test.txt"
 activityTrainFile <- "UCI HAR Dataset/train/y_train.txt"
 activityTestFile <- "UCI HAR Dataset/test/y_test.txt"
 
-subjectTrain <- as.vector(read.table(subjectTrainFile, colClasses = c("factor")))
-subjectTest <- as.vector(read.table(subjectTestFile, colClasses = c("factor")))
-activityTrain <- as.vector(read.table(activityTrainFile, colClasses = c("factor")))
-activityTest <- as.vector(read.table(activityTestFile, colClasses = c("factor")))
+subjectTrain <- as.vector(read.table(subjectTrainFile, colClasses = c("numeric"))[,1])
+subjectTest <- as.vector(read.table(subjectTestFile, colClasses = c("numeric"))[,1])
+activityTrain <- as.vector(read.table(activityTrainFile, colClasses = c("factor"))[,1])
+activityTest <- as.vector(read.table(activityTestFile, colClasses = c("factor"))[,1])
 
 # Add descriptive factor levels for activities. Use mapvalues() function from plyr package
 
 activityLabelsFile <- "UCI HAR Dataset/activity_labels.txt"
 activityLabels <- read.table(activityLabelsFile, colClasses = c("numeric", "character"), col.names = c("labNum", "label"))
-library(plyr)
-plyr::mapvalues(x, from = c("beta", "gamma"), to = c("two", "three"))
 
+library(plyr)
+activityTrain <- plyr::mapvalues(activityTrain, from = activityLabels$labNum, to = activityLabels$label)
+activityTest <- plyr::mapvalues(activityTest, from = activityLabels$labNum, to = activityLabels$label)
+
+# Create full training and test datasets by joining subject and activity info to the variables
+
+trainData <- cbind(subjectTrain,activityTrain,trainData)
+testData <- cbind(subjectTest,activityTest,testData)
+
+colnames(trainData) <- c("Subject", "Activity", cleanColNames)
+colnames(testData) <- c("Subject", "Activity", cleanColNames)
+
+fullData <- rbind(trainData, testData)
+
+# Preparing a tidy dataset
+
+library(reshape2)
+
+meltFullData <- melt(fullData,id=c("Subject","Activity"),measure.vars=colnames(fullData)[3:ncol(fullData)])
+tidyDataSet <- dcast(meltFullData, Subject + Activity~ variable,mean)
